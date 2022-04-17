7b91c58d559a9dd8ff8549fbfc1445b332f75cf5
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-17 13:25:12 -0500

    Button Test

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,12 +1,22 @@
 import { Message, Whatsapp } from "venom-bot";
 
-export default async function messagesHandler(message: Message, venom: Whatsapp){
-  if (message.body === "!comandos"){
-    venom.sendText(message.from,
-      "*COMANDOS*\n\n" +
-      "!stick - Genera un sticker con la imagen enviada\n"
-
-      )
+export default async function messagesHandler(
+  message: Message,
+  venom: Whatsapp
+) {
+  if (message.body === "!comandos") {
+    venom.sendText(
+      message.from,
+      "*COMANDOS*\n\n" + "!stick - Genera un sticker con la imagen enviada\n"
+    );
   }
 
+  //{ buttonText: { displayText: string; }
+  if (message.body === "!buttons") {
+    const buttons = [
+      { buttonId: "id1", buttonText: { displayText: "Button 1" } },
+      { buttonId: "id2", buttonText: { displayText: "Button 2" } },
+    ];
+    venom.sendButtons(message.from, "Elige una opción", buttons as [], "Hola");
+  }
 }
