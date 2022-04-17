34fe736ba83efd0613eb4b1176778a7de9286f8a
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-17 11:40:27 -0500

    Fixed  Lowercase Hell

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,8 +1,7 @@
-import * as _ from "lodash";
 import { Message, Whatsapp } from "venom-bot";
 
 export default async function messagesHandler(message: Message, venom: Whatsapp){
-  if (_.lowerCase(message.body) === "!comandos"){
+  if (message.body === "!comandos"){
     venom.sendText(message.from,
       "*COMANDOS*\n\n" +
       "!stick - Genera un sticker con la imagen enviada\n"
--- src/tools/ban.ts
+++ src/tools/ban.ts
@@ -1,12 +1,11 @@
-import * as _ from "lodash";
 import { Whatsapp } from "venom-bot";
 
 export async function banHandler(message: any, venom: Whatsapp) {
-  if (_.lowerCase(message.body) === "!ban" &&  message.quotedParticipant !== undefined && message.isGroup) {
+  if (message.body === "!ban" &&  message.quotedParticipant !== undefined && message.isGroup) {
     await venom.removeParticipant(message.chatId, message.quotedParticipant);
     await venom.sendText(message.chatId, `${message.quotedParticipant} ha sido baneado`);
     console.log("[BANNED] ",message.quotedParticipant)
   }
 
-  message.isGroup !== true && _.lowerCase(message.body) === "!ban" ? venom.sendText(message.from, "Este comando solo funciona en grupos") : null;
+  message.isGroup !== true && message.body === "!ban" ? venom.sendText(message.from, "Este comando solo funciona en grupos") : null;
 }
