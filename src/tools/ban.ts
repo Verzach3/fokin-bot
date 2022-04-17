6edbe2a6ae6b137ef8c9490da71a5f048ded58f5
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-17 11:45:54 -0500

    Try

--- src/tools/ban.ts
+++ src/tools/ban.ts
@@ -1,11 +1,12 @@
 import { Whatsapp } from "venom-bot";
 
 export async function banHandler(message: any, venom: Whatsapp) {
-  if (message.body === "!ban" &&  message.quotedParticipant !== undefined && message.isGroup) {
+  if (message.body === "!ban" &&  message.quotedParticipant !== undefined && !message.isGroup) {
     await venom.removeParticipant(message.chatId, message.quotedParticipant);
     await venom.sendText(message.chatId, `${message.quotedParticipant} ha sido baneado`);
     console.log("[BANNED] ",message.quotedParticipant)
   }
 
-  message.isGroup !== true && message.body === "!ban" ? venom.sendText(message.from, "Este comando solo funciona en grupos") : null;
+  
+  message.isGroup && message.body === "!ban" ? venom.sendText(message.from, "Este comando solo funciona en grupos") : null;
 }
