376b5141c3873f1287a7c954cae7e771e293f073
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-17 11:37:06 -0500

    Shit

--- src/tools/ban.ts
+++ src/tools/ban.ts
@@ -7,5 +7,6 @@ export async function banHandler(message: any, venom: Whatsapp) {
     await venom.sendText(message.chatId, `${message.quotedParticipant} ha sido baneado`);
     console.log("[BANNED] ",message.quotedParticipant)
   }
-  message.isGroup !== true ? venom.sendText(message.from, "Este comando solo funciona en grupos") : null;
+
+  message.isGroup !== true && _.lowerCase(message.body) === "!ban" ? venom.sendText(message.from, "Este comando solo funciona en grupos") : null;
 }
