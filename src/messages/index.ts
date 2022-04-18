7004f97662a6929d911ccd533b639b5a101cf8e0
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-18 09:45:56 -0500

    Updated !comandos

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -9,12 +9,12 @@ export default async function messagesHandler(
     venom.sendText(
       message.from,
       "*COMANDOS*\n\n" + 
-      "!comandos - Muestra este mensaje\n" +
-      "!stick - Genera un sticker con la imagen enviada\n" +
-      "!ban - Banea a el usuario mencionado usuario\n" +
+      "*!comandos* - Muestra este mensaje\n" +
+      "*!stick* - Genera un sticker con la imagen enviada\n" +
+      "*!ban* - Banea a el usuario mencionado usuario\n" +
       "_requiere permisos de administrador_\n" +
-      "!dlaudio link - Descarga el audio con el link*\n" +
-      "!dlvideo link - Descarga el video con el link*\n" +
+      "*!dlaudio* link - Descarga el audio con el link*\n" +
+      "*!dlvideo* link - Descarga el video con el link*\n" +
       "_solamente funciona con links de YouTube_\n" +
       "\n\n*trabajo el progreso"
     );
