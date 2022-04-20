e730da78dc77935fabe4a5113646392ce3196557
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-04-19 19:32:33 -0500

    removed !comandos

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -5,19 +5,7 @@ export default async function messagesHandler(
   message: Message,
   venom: Whatsapp
 ) {
-  if (message.body === "!comandos") {
-    venom.sendText(
-      message.from,
-      "*COMANDOS*\n\n" + 
-      "*!comandos* - Muestra este mensaje\n" +
-      "*!stick* - Genera un sticker con la imagen enviada\n" +
-      "*!ban* - Banea a el usuario mencionado usuario\n" +
-      "_requiere permisos de administrador_\n" +
-      "*!dlaudio* link - Descarga el audio con el link*\n" +
-      "*!dlvideo* link - Descarga el video con el link*\n" +
-      "_solamente funciona con links de YouTube_\n" +
-      "\n\n*trabajo el progreso"
-    );
-  }
+
 
 }
+
